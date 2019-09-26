1ea0a9a9311bdafb401b80539952ed046e7308a0
unknown <alex_s253@hotmail.com>
2019-09-26 20:11:42 +0930

    installed express handlebars, begining to change things

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require ('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+//express middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Static folder
+
+app.get('/about', (req, res) =>
+  res.render('about', {
+    title: 'Update',
+    name: 'Alex',
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT  = process.env.PORT || 5000;
