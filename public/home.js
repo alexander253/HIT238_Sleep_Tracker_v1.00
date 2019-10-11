769151b7b2affe6e6da9aab7bda4b7c090720ade
unknown <alex_s253@hotmail.com>
2019-10-12 02:44:58 +0930

    added a congrats msg and tip of the day. added css animations

--- public/home.js
+++ public/home.js
@@ -9,6 +9,7 @@ hours.innerHTML= localStorage.getItem("hoursTracked")
 
 
 const moreSleep=parseInt(localStorage.getItem("hoursTracked"));
+const goalHours=parseInt(localStorage.getItem("goalz"));
 
 console.log(moreSleep);
 
@@ -20,3 +21,12 @@ if (moreSleep<8 ){
   document.getElementById('moreSleepTip').innerHTML=("Make sure you keep it up on a regular basis");
 
 };
+
+if (moreSleep==goalHours){
+  document.getElementById('meetGoal').innerHTML=("Congratulations!");
+  document.getElementById('meetGoalMsg').innerHTML=("You have met your goals!");
+} else {
+  document.getElementById('meetGoal').innerHTML=("Tip of the day:");
+  document.getElementById('meetGoalMsg').innerHTML=("Try to limit your exercise before bed");
+
+}
