57c89af309c2c38b8f726948cddbd058d4f8a0bf
unknown <alex_s253@hotmail.com>
2019-10-12 00:45:57 +0930

    added if statement if hours slept is lower than 8 a different message will appear

--- public/home.js
+++ public/home.js
@@ -6,3 +6,17 @@ const hours = document.getElementById("hours")
 outName.innerHTML= localStorage.getItem("Name");
 sleepGoalHours.innerHTML= localStorage.getItem("goalz");
 hours.innerHTML= localStorage.getItem("hoursTracked")
+
+
+const moreSleep=parseInt(localStorage.getItem("hoursTracked"));
+
+console.log(moreSleep);
+
+if (moreSleep<8 ){
+  document.getElementById('moreSleep').innerHTML=("Looks like you need more sleep");
+  document.getElementById('moreSleepTip').innerHTML=("Have you tried counting sheep?");
+}else {
+  document.getElementById('moreSleep').innerHTML=("Looks like you are getting enough sleep");
+  document.getElementById('moreSleepTip').innerHTML=("Make sure you keep it up on a regular basis");
+
+};
