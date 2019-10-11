8da0ec2f094f848eb490fb19b7a620c4c33c4616
unknown <alex_s253@hotmail.com>
2019-10-12 00:11:46 +0930

    added track functionality and sleep hour goals on home page.

--- public/about.js
+++ public/about.js
@@ -1,7 +1,5 @@
 const inpName = document.getElementById("nameInput");
 const saveBtn = document.getElementById("saveBtn");
-const outName = document.getElementById("name");
-const sleepGoalHours = document.getElementById("goal");
 const key= "Name";
 const sleepGoal = "goalz";
 
@@ -11,8 +9,6 @@ document.getElementById("saveBtn").addEventListener("click", save);
 
 function save(){
 const value = inpName.value;
-
-console.log(value);
 
 if (key && value){
   localStorage.setItem(key, value);
@@ -25,8 +21,6 @@ document.getElementById("hourBtn").addEventListener("click", saveGoal);
 
 function saveGoal(){
 const hours = hourInp.value;
-
-console.log(hours);
 
 if (sleepGoal && hours){
   localStorage.setItem(sleepGoal, hours);
--- public/home.js
+++ public/home.js
@@ -1,7 +1,8 @@
 const outName = document.getElementById("name");
 const sleepGoalHours = document.getElementById("goal");
-
+const hours = document.getElementById("hours")
 
 
 outName.innerHTML= localStorage.getItem("Name");
 sleepGoalHours.innerHTML= localStorage.getItem("goalz");
+hours.innerHTML= localStorage.getItem("hoursTracked")
